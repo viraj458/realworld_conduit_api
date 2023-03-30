34ac50a95a82a1e908a985717774406e05c25da3
Viraj <virajmalaka94@gmail.com>
2023-03-30 17:26:55 +0530

    feat: created get current user feature

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,17 @@
+import db from '../db.js'
+
 //get current user
 export const getCurrentUser = async(req, res) => {
-    
+    try {
+        const {authorization} = req.headers
+        const token = authorization.split(' ')[1]
+
+        const {id} = req.user
+        const user = await db('users').where({id}).first()
+        // console.log(token);
+        
+        res.status(200).json({user:{email: user.email, token, username: user.username }})
+    } catch (err) {
+        res.status(400).json({error: err.message})
+    }
 }
