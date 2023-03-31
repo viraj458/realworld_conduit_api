ebe81fcfd96077913382af928a944ddcb031905a
Viraj <virajmalaka94@gmail.com>
2023-03-31 16:48:28 +0530

    fix: error while sending post request to add article details

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -1,5 +1,7 @@
 import db from '../db.js'
+import slugify from 'slugify'
 
+//create article
 export const createArticle = async(req, res) => {
     try {
 
@@ -9,25 +11,56 @@ export const createArticle = async(req, res) => {
 
         const {body,description,title,tagList} = req.body.article
         const tag_list = JSON.stringify(tagList)
+
+        const slug = slugify(title);
         
         // console.log(tagList);
 
         const [article] = await db('articles').insert({
+            slug,
             body,
             description,
             title,
             tagList: tag_list,
-            author: {
+            author:{
                 username: user.username,
                 bio: user.bio,
                 image: user.image,
                 following: false
             }
         })
-        console.log(article);
 
-        res.status(200).json(article)
+        
+
+        const [createdArticle] = await db('articles').where('id', article).select('*')
+        console.log(createdArticle);
+
+        res.status(200).json({article:{
+            slug,
+            body,
+            description,
+            title,
+            tagList: JSON.parse(tag_list),
+            author:{
+                username: user.username,
+                bio: user.bio,
+                image: user.image,
+                following: false
+            }
+        }})
     } catch (err) {
         res.status(400).json({error:err.message})
     }
+}
+
+
+//get all articles
+
+export const getAllArticles = async(req, res) => {
+    
+}
+
+//delete article
+export const deleteArticle = async(req, res) => {
+    
 }
--- routes/articleRoute.js
+++ routes/articleRoute.js
@@ -1,11 +1,17 @@
 import express from 'express'
-import { createArticle } from '../controllers/articleController.js'
+import { createArticle, deleteArticle, getAllArticles } from '../controllers/articleController.js'
 import auth from '../middlewares/auth.js'
 
 
 const router = express.Router()
 
 //create an article
-router.post('/', auth,  createArticle)
+router.post('/', auth, createArticle)
+
+//create an article
+router.get('/', getAllArticles)
+
+//delete an article
+router.delete('/:slug', auth, deleteArticle)
 
 export default router
