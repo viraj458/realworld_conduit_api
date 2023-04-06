1ec80cf1aae5f3b55ff6e35b6b216af7bc0fa427
Viraj <virajmalaka94@gmail.com>
2023-04-06 16:02:47 +0530

    feat: created unfavourite article feature

--- controllers/favouriteController.js
+++ controllers/favouriteController.js
@@ -20,12 +20,13 @@ export const favouriteArticle = async(req, res) => {
         const favouriteArticlesInt = favouriteArticles.map(elem=>parseInt(elem))
         // console.log(favouriteArticlesInt);
 
-        if(favouriteArticlesInt.indexOf(article.id)===-1){
+        const index =favouriteArticlesInt.indexOf(article.id) 
+        if(index===-1){
             favouriteArticles.push(article.id)
 
             //update the favourite count on a specific article
             await db('articles').where({slug}).increment('favouriteCount', 1)
-            console.log(article);
+            // console.log(article);
         }
 
         await db('users').where({id}).update({favouriteArticles: favouriteArticles.join(',')})
@@ -47,6 +48,7 @@ export const favouriteArticle = async(req, res) => {
             tagList: JSON.parse(articleInfo.tagList),
             createdAt: articleInfo.createdAt,
             updatedAt: articleInfo.updatedAt,
+            favorited: index===-1 ? true : false,
             favoritesCount: articleInfo.favouriteCount,
             author:{
               username: articleInfo.username,
@@ -63,5 +65,55 @@ export const favouriteArticle = async(req, res) => {
 }
 
 export const unFavouriteArticle = async(req, res) => {
+
+    try {
+        const {id} = req.user
+        const {slug} = req.params
+
+        const article = await db('articles').where({slug}).select('id').first()
+
+        const favourite = await db('users').where({id}).select('favouriteArticles').first()
+
+        const favouriteArticles = favourite.favouriteArticles ? favourite.favouriteArticles.split(',') : [];
+
+        const favouriteArticlesInt = favouriteArticles.map(elem=>parseInt(elem))
+
+        const index = favouriteArticlesInt.indexOf(article.id)
+        if(index!==-1){
+            favouriteArticles.splice(index, 1)
+
+            await db('articles').where({slug}).decrement('favouriteCount', 1)
+            
+        }
+
+        await db('users').where({id}).update({favouriteArticles: favouriteArticles.join(',')})
+
+        const [articleInfo] = await db('articles')
+            .where({slug})
+            .join('users', 'articles.author', 'users.id')
+            .select('articles.*', 'users.username', 'users.bio', 'users.image')
+
+            
+
+            res.status(200).json({article:{
+                slug: articleInfo.slug,
+                body: articleInfo.body,
+                description: articleInfo.description,
+                title: articleInfo.title,
+                tagList: JSON.parse(articleInfo.tagList),
+                createdAt: articleInfo.createdAt,
+                updatedAt: articleInfo.updatedAt,
+                favorited: index!==-1 ? false : true,
+                favoritesCount: articleInfo.favouriteCount,
+                author:{
+                username: articleInfo.username,
+                bio: articleInfo.bio,
+                image: articleInfo.image,
+                following: false
+                }
+            }})
+    } catch (err) {
+        res.status(400).json({error: err.message})
+    }
     
 }
